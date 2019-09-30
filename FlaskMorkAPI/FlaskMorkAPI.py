71db9b390e76ae0ef50f8b2f73574b7ef823de0b
Tshen1994 <shenshenshener@163.com>
2019-09-30 09:20:12 +0800

    Commit All

---
+++ FlaskMorkAPI/FlaskMorkAPI.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import json
+import random
+
+app = Flask(__name__)
+
+@app.route('/GET/<int:Factor>',methods=['GET'])
+def getPrice(Factor):
+    Price = random.randint(0, Factor+1)
+    Face = random.randint(Factor, 2*Factor)
+    dict = {}
+    dict['Price'] = Price
+    dict['Face'] = Face
+    return json.dumps(dict)
+
+@app.route('/POST',methods=['POST'])
+def getResult():
+    j_data = request.json
+    price = j_data['Price']
+    face = j_data['Face']
+    dict={}
+    dict['Price'] = price
+    dict['Face'] = face
+    dict['Result'] = price*face
+    return json.dumps(dict)
+
+if __name__ == '__main__':
+    app.run(debug=True)
